229d7076823b376f3a77a3be2dfcfdc8f8ddbc17
cjsdn9308 <84078407+cjsdn9308@users.noreply.github.com>
2023-03-28 14:12:27 +0900

    Add files via upload

---
+++ PDF_Read/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace PDF_Read
+{
+    partial class PDF_Read
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.txtRseult = new System.Windows.Forms.TextBox();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnOpenFile.Location = new System.Drawing.Point(3, 483);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(1079, 48);
+            this.btnOpenFile.TabIndex = 0;
+            this.btnOpenFile.Text = "열기";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            // 
+            // txtRseult
+            // 
+            this.txtRseult.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtRseult.Location = new System.Drawing.Point(3, 3);
+            this.txtRseult.Multiline = true;
+            this.txtRseult.Name = "txtRseult";
+            this.txtRseult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtRseult.Size = new System.Drawing.Size(1079, 474);
+            this.txtRseult.TabIndex = 1;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.txtRseult, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnOpenFile, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1085, 534);
+            this.tableLayoutPanel1.TabIndex = 2;
+            // 
+            // PDF_Read
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1085, 534);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "PDF_Read";
+            this.Text = "PDF_Read";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.TextBox txtRseult;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
+
---
+++ PDF_Read/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+namespace PDF_Read
+{
+    public partial class PDF_Read : Form
+    {
+        public PDF_Read()
+        {
+            InitializeComponent();
+
+            this.btnOpenFile.Click += new EventHandler(btnOpenFile_Click);
+        }
+
+        void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.ShowDialog();
+
+            if (fd.FileName != "")
+            {
+                string str = Ex(fd.FileName);
+
+                if (!string.IsNullOrEmpty(str))
+                {
+                    //string[] arr = str.Split("Eone");
+                    //string splstrF = arr[1].ToString().Substring(3);
+
+                    this.txtRseult.Text = str.Replace("\n",Environment.NewLine);
+                }
+            }
+        }
+
+        private string Ex(string sPath)
+        {
+            StringBuilder result = new StringBuilder();
+            //StringBuilder text = new StringBuilder();
+            try
+            {
+                using (Stream newpdfStream = new FileStream(sPath, FileMode.Open, FileAccess.Read))
+                {
+                    PdfReader pdfReader = new PdfReader(newpdfStream);
+
+                    for (int i = 1; i <= pdfReader.NumberOfPages; i++)
+                    {
+                        result.Append(PdfTextExtractor.GetTextFromPage(pdfReader, i, new iTextSharp.text.pdf.parser.SimpleTextExtractionStrategy())).Append("\r\n\r\n\r\n");
+
+
+                        //ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
+                        //string currentText = PdfTextExtractor.GetTextFromPage(pdfReader, i, strategy);
+
+                        //currentText = Encoding.UTF8.GetString(ASCIIEncoding.Convert(Encoding.Default, Encoding.UTF8, Encoding.Default.GetBytes(currentText)));
+                        //text.Append(currentText);
+                    }
+                    //pdfReader.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return result.ToString();
+        }
+    }
+}
